75780e07bcc57f696e83300408f691151a150fa8
mattmrq <marquismat@gmail.com>
2023-10-21 21:11:19 +0200

    pydantic file

--- src/web_service/lib/models.py
+++ src/web_service/lib/models.py
@@ -1,1 +1,18 @@
 # Pydantic models for the web service
+from pydantic import BaseModel
+
+
+class InputData(BaseModel):
+    Sex: str
+    Length: float
+    Diameter: float
+    Height: float
+    Whole_weight: float
+    Shucked_weight: float
+    Viscera_weight: float
+    Shell_weight: float
+    Rings: int
+
+
+class OutputData(BaseModel):
+    Age: int
