c45ee5b21eb5b39ea1270bde2622cb7297f14508
arthurprgt <arthur.perrigot0@gmail.com>
2023-10-23 16:17:01 +0200

    Created workflows and tasks for prefect

--- config/config.py
+++ config/config.py
@@ -1,10 +1,11 @@
 from pathlib import Path
 
-DATA_PATH = 'data'
-TRAIN_PATH = Path(DATA_PATH, 'abalone_train.csv')
-TEST_PATH = Path(DATA_PATH, 'abalone_test.csv')
-PREDICT_PATH = Path(DATA_PATH, 'abalone_test.csv')
+PROJECT_ROOT = Path(__file__).resolve().parents[3]
+DATA_PATH = str(PROJECT_ROOT / 'data')
+TRAIN_PATH = DATA_PATH + '/abalone_train.csv'
+TEST_PATH = DATA_PATH + '/abalone_test.csv'
+PREDICT_PATH = DATA_PATH + '/abalone_predict.csv'
 
-MODEL_PATH = 'src/web_service/local_object'
+MODEL_PATH = 'src/web_service/local_objects'
 
 CATEGORICAL_COLS = ["Sex"]
