062ce8075fc4c4e97e29b29db439eaac3ab50b89
Aristide Gasquet <116149405+arigqt@users.noreply.github.com>
2023-10-23 12:10:44 +0200

    Create app_config.py

---
+++ src/web_service/app_config.py
@@ -0,0 +1,16 @@
+# MODELS
+MODEL_VERSION = "0.0.1"
+PATH_TO_PREPROCESSOR = f"local_models/dv__v{MODEL_VERSION}.pkl"
+PATH_TO_MODEL = f"local_models/model__v{MODEL_VERSION}.pkl"
+CATEGORICAL_VARS = ["PULocationID", "DOLocationID", "passenger_count"]
+
+
+# MISC
+APP_TITLE = "TripDurationPredictionApp"
+APP_DESCRIPTION = (
+    "A simple API to predict abalone ages given"
+    "different features extracted from a Kaggle dataset."
+    "This app aims to simulate a ML industrialization for"
+    "our MLOPS class"
+)
+APP_VERSION = "0.0.1"
