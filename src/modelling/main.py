3ae789a6504f2dfb5573226133b7226d9d728af0
arthurprgt <arthur.perrigot0@gmail.com>
2023-10-23 14:27:24 +0200

    Creating python scripts for deployment

--- src/modelling/main.py
+++ src/modelling/main.py
@@ -1,19 +1,26 @@
 # This module is the training flow: it reads the data, preprocesses it, trains a model and saves it.
 
 import argparse
+from pathlib import Path
+from loguru import logger
+from training import train_model
+from preprocessing import process_data
+from utils import save_pickle
 
 
 def main(trainset_path: Path) -> None:
     """Train a model using the data at the given path and save the model (pickle)."""
     # Read data
-
-    # Preprocess data
-
+    logger.info("Processing training data...")
+    X_train, y_train, dv = process_data(filepath=trainset_path)
     # (Optional) Pickle encoder if need be
-
+    save_pickle("src/web_service/local_objects" + "/dv.pkl", dv)
     # Train model
-
+    logger.info("Training model...")
+    model = train_model(X_train, y_train)
     # Pickle model --> The model should be saved in pkl format the `src/web_service/local_objects` folder
+    save_pickle("src/web_service/local_objects" + "/model.pkl", model)
+    logger.info("Done !")
 
 
 if __name__ == "__main__":
--- src/modelling/utils.py
+++ src/modelling/utils.py
@@ -1,1 +1,13 @@
 # Use this module to code a `pickle_object` function. This will be useful to pickle the model (and encoder if need be).
+import pickle
+
+def save_pickle(path: str, obj:dict):
+    """
+    Save an object as a pickled file.
+
+    Args:
+        path (str): The path to save the pickle file.
+        obj (dict): The object to be pickled and saved.
+    """
+    with open(path, "wb") as f:
+        pickle.dump(obj, f)
